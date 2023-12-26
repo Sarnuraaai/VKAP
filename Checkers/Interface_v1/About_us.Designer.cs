329fab1a6cc2dfbd3e71b7a673f62e07f127a1ff
Андрей Преснухин <andrey.presnukhin@gmail.com>
2023-12-27 01:09:49 +0300

    [PR-11] Создание окна О разработчике

---
+++ Checkers/Interface_v1/About_us.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace Interface_v1
+{
+    partial class About_us
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About_us));
+            SuspendLayout();
+            // 
+            // About_us
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.About_us;
+            ClientSize = new Size(350, 699);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "About_us";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "ABOUT US";
+            FormClosed += About_us_FormClosed;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Checkers/Interface_v1/About_us.cs
@@ -0,0 +1,51 @@
+﻿/**
+    @file About_us.cs
+    @brief Файл о разработчике игры Шашки
+    @copyright VKAP
+    @author Красильников В.Г. и Преснухин А.П.
+    @date 27-12-2023
+    @version 1.1.20
+\par Использует класс:
+    @ref About_us
+\par Содержит класс:
+    @ref About_us
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interface_v1
+{
+    /**
+    @brief Класс About_us
+    
+    Основной и единственный класс, отвечающий за отображение информации о разработчике игры
+    */
+    public partial class About_us : Form
+    {
+
+        /// Метод инициализации
+        /** Инициализация всех компонентов окна О разработчике
+        */
+        public About_us()
+        {
+            InitializeComponent();
+        }
+
+        /// Метод, показывающий окно О разработчиках
+        /** 
+        */
+        private void About_us_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Form Interface = Application.OpenForms[0];
+            Interface.Show();
+        }
+    }
+}
